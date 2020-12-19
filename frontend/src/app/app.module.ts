de631d8fcfcff3a2dcab5ac48fb09febf507fbb1
SergeySerg123 <sergey_chizhik@ukr.net>
2020-12-19 11:26:09 +0200

    Init basic components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MainBoardComponent } from './components/main-board/main-board.component';
+import { SubBoardComponent } from './components/sub-board/sub-board.component';
+import { CreateButtonComponent } from './components/create-button/create-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainBoardComponent,
+    SubBoardComponent,
+    CreateButtonComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/components/sub-board/sub-board.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sub-board',
+  templateUrl: './sub-board.component.html',
+  styleUrls: ['./sub-board.component.scss']
+})
+export class SubBoardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
