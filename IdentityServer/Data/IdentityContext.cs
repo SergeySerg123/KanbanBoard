2232ccdbf0fc855ce9f800025ac680803a0cb22e
SergeySerg123 <sergey_chizhik@ukr.net>
2020-12-24 00:16:49 +0200

    Create IdentityContext.cs

---
+++ IdentityServer/Data/IdentityContext.cs
@@ -0,0 +1,20 @@
+﻿using IdentityServer.Data.Entities;
+using IdentityServer.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityServer.Data
+{
+    public class IdentityContext : DbContext
+    {
+        public IdentityContext(DbContextOptions<IdentityContext> options) : base(options) { }
+
+        public DbSet<RefreshToken> RefreshTokens { get; private set; }
+        public DbSet<User> Users { get; private set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<RefreshToken>().Ignore(t => t.IsActive);
+        
+        }
+    }
+}
