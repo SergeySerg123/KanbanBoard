1f7a20b08899e525f4685475225a2cd7cfeb6057
SergeySerg123 <sergey_chizhik@ukr.net>
2020-12-24 00:16:20 +0200

    Entities

---
+++ IdentityServer/Data/Entities/RefreshToken.cs
@@ -0,0 +1,24 @@
+﻿using IdentityServer.Data.Entities.Abstract;
+using IdentityServer.Entities;
+using System;
+
+namespace IdentityServer.Data.Entities
+{
+    public sealed class RefreshToken : BaseEntity
+    {
+        private const int DAYS_TO_EXPIRE = 5;
+
+        public RefreshToken()
+        {
+            Expires = DateTime.UtcNow.AddDays(DAYS_TO_EXPIRE);
+        }
+
+        public string Token { get; set; }
+        public DateTime Expires { get; private set; }
+
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+        public bool IsActive => DateTime.UtcNow <= Expires;
+    }
+}
---
+++ IdentityServer/Data/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson;
+
+namespace IdentityServer.Entities
+{
+    public class User
+    {
+        public ObjectId Id { get; set; }
+        public string URL { get; set; }
+
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Salt { get; set; }
+
+    }
+}
