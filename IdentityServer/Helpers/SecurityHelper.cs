84da51398adbaff689eaa6aef2d1a6e20bc67e28
SergeySerg123 <sergey_chizhik@ukr.net>
2020-12-24 00:15:49 +0200

    Create SecurityHelper.cs

---
+++ IdentityServer/Helpers/SecurityHelper.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace IdentityServer.Helpers
+{
+    public static class SecurityHelper
+    {
+        public static string HashPassword(string password, byte[] salt)
+        => Convert.ToBase64String(
+                    KeyDerivation.Pbkdf2(
+                        password: password,
+                        salt: salt,
+                        prf: KeyDerivationPrf.HMACSHA256,
+                        iterationCount: 10000,
+                        numBytesRequested: 256 / 8
+                    )
+                );
+
+        public static byte[] GetRandomBytes(int length = 32)
+        {
+            using (var randomNumberGenerator = new RNGCryptoServiceProvider())
+            {
+                var salt = new byte[length];
+                randomNumberGenerator.GetBytes(salt);
+
+                return salt;
+            }
+        }
+        public static string GetRandomToken(int length = 32)
+        {
+            const string chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+            var sb = new StringBuilder();
+            Random rnd = new Random();
+
+            for (int i = 0; i < length; i++)
+            {
+                int index = rnd.Next(chars.Length);
+                sb.Append(chars[index]);
+            }
+
+            return sb.ToString();
+        }
+
+        public static bool ValidatePassword(string password, string hash, string salt)
+        {
+            return HashPassword(password, Convert.FromBase64String(salt)) == hash;
+        }
+    }
+}
