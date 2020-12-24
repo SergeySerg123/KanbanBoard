a800b141f51f9ece5180c255215de70ad3eeb786
SergeySerg123 <sergey_chizhik@ukr.net>
2020-12-24 14:25:34 +0200

    Exceptions
    Create custom Exceptions

---
+++ backend/KanbanBoard.Api/Exceptions/ExpiredRefreshTokenException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace KanbanBoard.Api.Exceptions
+{
+    public sealed class ExpiredRefreshTokenException : Exception
+    {
+        public ExpiredRefreshTokenException() : base("Refresh token expired.") { }
+    }
+}
---
+++ backend/KanbanBoard.Api/Exceptions/InvalidConfirmPasswordTokenException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace KanbanBoard.Api.Exceptions
+{
+    public sealed class InvalidConfirmPasswordTokenException : Exception
+    {
+        public InvalidConfirmPasswordTokenException() : base("Invalid confirm password token exception.") { }
+    }
+}
