c91ba8d1149f206d3590ea02b31f6ebcc7a658c6
SergeySerg123 <sergey_chizhik@ukr.net>
2020-12-24 14:26:10 +0200

    Create RegisterController.cs

---
+++ IdentityServer/Controllers/RegisterController.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Tasks;
+using IdentityServer.DTO.User;
+using IdentityServer.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityServer.Controllers
+{
+    [Route("api/[controller]")]
+    [AllowAnonymous]
+    [ApiController]
+    public class RegisterController : ControllerBase
+    {
+        private readonly UserService _userService;
+        private readonly AuthService _authService;
+
+        public RegisterController(UserService userService, AuthService authService)
+        {
+            _userService = userService;
+            _authService = authService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] UserRegisterDTO user)
+        {
+            var createdUser = await _userService.CreateUser(user);
+            var token = await _authService.GenerateAccessToken(createdUser.Id, createdUser.UserName, createdUser.Email);
+
+            var result = new AuthUserDTO
+            {
+                User = createdUser,
+                Token = token
+            };
+
+            return CreatedAtAction("GetById", "users", new { id = createdUser.Id }, result);
+        }
+
+    }
+}
