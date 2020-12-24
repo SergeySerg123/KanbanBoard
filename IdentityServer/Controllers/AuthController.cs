e3e2ea829e5f89e30f1a5720de4233c4b9d7d7ea
SergeySerg123 <sergey_chizhik@ukr.net>
2020-12-24 14:26:08 +0200

    Update AuthController.cs

--- IdentityServer/Controllers/AuthController.cs
+++ IdentityServer/Controllers/AuthController.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
+using IdentityServer.DTO.User;
+using IdentityServer.Services;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IdentityServer.Controllers
@@ -14,12 +12,10 @@ namespace IdentityServer.Controllers
     public class AuthController : ControllerBase
     {
         private readonly AuthService _authService;
-        private readonly EmailService _emailService;
 
-        public AuthController(AuthService authService, EmailService emailService)
+        public AuthController(AuthService authService)
         {
             _authService = authService;
-            _emailService = emailService;
         }
 
         [HttpPost("login")]
@@ -27,15 +23,6 @@ namespace IdentityServer.Controllers
         public async Task<ActionResult<AuthUserDTO>> Login(UserLoginDTO dto)
         {
             return Ok(await _authService.Authorize(dto));
-        }
-
-        [HttpGet("password/reset")]
-        [AllowAnonymous]
-        public async Task<ActionResult> Reset(string token)
-        {
-            var confirmToken = await _authService.Reset(token);
-            var url = $"http://{Request.Host.Host + ":" + 4200}/profile?confirmToken={confirmToken}";
-            return Redirect(url);
         }
     }
 }
