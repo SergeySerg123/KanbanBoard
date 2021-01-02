4521c6e883f145815a4e0656975c17401b10a0ac
SergeySerg123 <sergey_chizhik@ukr.net>
2021-01-02 20:07:19 +0200

    Create GoalProfile.cs

---
+++ backend/Src/Services/KanbanBoard.Api/MappingProfiles/GoalProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using KanbanBoard.Api.DTO;
+using KanbanBoard.Api.Models;
+using MongoDB.Bson;
+
+namespace KanbanBoard.Services.Goals.Api.MappingProfiles
+{
+    public class GoalProfile : Profile
+    {
+        public GoalProfile()
+        {
+            CreateMap<GoalDTO, Goal>()
+                .ForMember(c => c.AuthorId, src => src.MapFrom(x => new ObjectId(x.AuthorId)))
+                .ForMember(c => c.BoardId, src => src.MapFrom(x => new ObjectId(x.BoardId)));
+
+            CreateMap<Goal, GoalDTO>()
+                .ForMember(c => c.AuthorId, src => src.MapFrom(x => x.AuthorId.ToString()))
+                .ForMember(c => c.BoardId, src => src.MapFrom(x => x.BoardId.ToString()));
+        }
+    }
+}
